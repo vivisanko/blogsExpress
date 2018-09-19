601cbb913de025b7b373753e55ea2b33733008e3
Victoria Sanko <v.sanko@godeltech.com>
2018-09-19 16:20:20 +0300

    logger

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const fs = require('fs');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
@@ -11,13 +12,20 @@ const blogsRouter = require('./routes/blogs');
 
 const app = express();
 
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {
+  flags: 'a'
+})
 // console.log('db',db);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+app.use(logger('combined', {
+  stream: accessLogStream
+}));
+
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({
   extended: false
